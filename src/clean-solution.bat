522bca1031c73d852dc2333fdc1e7b00f5a1a58b
Dimitrie Tataru <dimitriettr@gmail.com>
2024-04-08 12:57:58 +0300

    Update 'clean-solution' script

--- src/clean-solution.bat
+++ src/clean-solution.bat
@@ -1,10 +1,10 @@
 rmdir /s /q .\Ace.CSharp.DataFaker\bin
 rmdir /s /q .\Ace.CSharp.DataFaker\obj
 
-rmdir /s /q .\Ace.CSharp.DataFaker.Sample\bin
-rmdir /s /q .\Ace.CSharp.DataFaker.Sample\obj
-
 rmdir /s /q .\Ace.CSharp.DataFaker.Tests\bin
 rmdir /s /q .\Ace.CSharp.DataFaker.Tests\obj
+
+rmdir /s /q .\sample\Ace.CSharp.DataFaker.Sample\bin
+rmdir /s /q .\sample\Ace.CSharp.DataFaker.Sample\obj
 
 pause
